3f19f5ef3c6c4375d43833917c5918cdbc06dfbb
Ryuu Mitsuki <dhefam31@gmail.com>
2025-11-11 03:33:29 +0700

    refactor(utils): Improve the `isValidURL` function
    Now the function can check more strict for given URL.
    
    - Checks if the `str` is a string (URL string)
    - Checks if the `str` is not a nullable value
    - Checks if the `str` is a valid URL by passing into `URL` class

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17,10 +17,25 @@ module.exports = {
         return `[${progressText}${emptyText}]`;
     },
 
-    isValidURL(string) {
+    /**
+     * Check whether the given URL is valid.
+     *
+     * @param {string} str - The URL string.
+     * @returns {boolean} `true` if the given URL is valid; `false` otherwise.
+     */
+    isValidURL(str) {
+        if (typeof str !== 'string') return false;  // not a string is invalid
+        const trimmed = str.trim();
+        if (!trimmed) return false;  // empty string is invalid URL
         try {
-            new URL(string);
+            new URL(trimmed);
             return true;
+
+            // ? Should we just use this logic below?
+            // ? It provides better detection for HTTP and HTTPS protocols
+            // ? which is the valid for website URLs
+            // ---
+            // return url.protocol === 'http:' || url.protocol === 'https:';
         } catch (_) {
             return false;
         }
